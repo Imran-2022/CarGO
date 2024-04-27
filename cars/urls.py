cceaa7c49518dbab158fba21a20609bbb527d65f
Imran-2022 <mdimranulhaque202@gmail.com>
2024-04-27 16:09:12 +0600

    add class based view

--- cars/urls.py
+++ cars/urls.py
@@ -1,13 +1,30 @@
-from django.contrib import admin
-from django.urls import path,include
-from . import views
+# from django.contrib import admin
+# from django.urls import path,include
+# from . import views
+
+# urlpatterns = [
+#     path('add/',views.add_post,name='add_post'),
+#     path('car_details/<int:id>',views.car_details,name='car_details'),
+#     path('profile/',views.order_history,name='profile'),
+#     path('add_comment/<int:id>/', views.add_comment, name='add_comment'),
+#     path('buy_now/',views.buy_now,name='buy_now'),
+#     path('edit/<int:id>',views.edit_post,name='edit_post'),
+#     path('delete/<int:id>',views.delete_post,name='delete_post')
+# ]
+
+
+# convert into class based view
+
+from django.urls import path
+from .views import AddPostView, EditPostView, CarDetailsView, DeletePostView, BuyNowView, AddCommentView,order_history
 
 urlpatterns = [
-    path('add/',views.add_post,name='add_post'),
-    path('car_details/<int:id>',views.car_details,name='car_details'),
-    path('profile/',views.order_history,name='profile'),
-    path('add_comment/<int:id>/', views.add_comment, name='add_comment'),
-    path('buy_now/',views.buy_now,name='buy_now'),
-    path('edit/<int:id>',views.edit_post,name='edit_post'),
-    path('delete/<int:id>',views.delete_post,name='delete_post')
+    path('add/', AddPostView.as_view(), name='add_post'),
+    path('edit/<int:id>/', EditPostView.as_view(), name='edit_post'),
+    path('car_details/<int:id>/', CarDetailsView.as_view(), name='car_details'),
+    path('delete/<int:id>/', DeletePostView.as_view(), name='delete_post'),
+    path('buy_now/', BuyNowView.as_view(), name='buy_now'),
+    # path('profile/', OrderHistoryView.as_view(), name='profile'),
+    path('profile/',order_history,name='profile'),
+    path('add_comment/<int:id>/', AddCommentView.as_view(), name='add_comment'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,65 +1,158 @@
-from django.shortcuts import render,redirect
-from . import forms
-from . import models
+# from django.shortcuts import render,redirect
+# from . import forms
+# from . import models
+# from django.contrib.auth.decorators import login_required
+# from django.shortcuts import get_object_or_404, render
+# from .models import Post,Order,Comment
+# from django.contrib import messages
+# # Create your views here.
+
+# @login_required
+# def add_post(request):
+#     if request.method=='POST':
+#         post_form=forms.PostForm(request.POST)
+#         if post_form.is_valid():
+#             new_post = post_form.save(commit=False)  # Create a new Post object but don't save to database yet
+#             new_post.author = request.user  # Assign the author
+#             new_post.save()  # Save the post with the assigned author
+#             return redirect('add_post')
+#     else:
+#         post_form=forms.PostForm()
+#     return render(request, 'add_post.html',{'form':post_form})
+
+# @login_required
+# def edit_post(request,id):
+#     # post model er instance (/object/children) dakhte parbe ! 
+
+#     post=models.Post.objects.get(pk=id)
+#     # print(post)
+#     post_form=forms.PostForm(instance=post)
+
+#     if request.method=='POST':
+#         post_form=forms.PostForm(request.POST,instance=post)
+#         # user kno kichu change na krle instance... !
+#         if post_form.is_valid():
+#             post_form.instance.author=request.user
+#             post_form.save()
+#             return redirect('homepage')
+#     return render(request, 'add_post.html',{'form':post_form})
+
+
+# def car_details(request, id):
+#     post = get_object_or_404(Post, pk=id)
+#     return render(request, 'car_details.html', {'car': post})
+
+# @login_required
+# def delete_post(request,id):
+#     post=models.Post.objects.get(pk=id)
+#     post.delete()
+#     return redirect('homepage')
+# @login_required
+# def buy_now(request):
+#     if request.method == 'POST' and request.user.is_authenticated:
+#         car_id = request.POST.get('car_id')
+#         if car_id:
+#             # Get the car object
+#             car = Post.objects.get(id=car_id)
+#             # Create a new order for the current user
+#             Order.objects.create(car=car, user=request.user)
+#             # Decrease the car quantity
+#             car.car_quantity -= 1
+#             car.save()
+#     return redirect('profile')  # Redirect to order history page or user profile
+
+# @login_required
+# def order_history(request):
+#     orders = Order.objects.filter(user=request.user)
+#     context = {'orders': orders}
+#     print(context)
+#     return render(request, 'profile.html', context)
+
+
+# @login_required
+# def add_comment(request, id):
+#     if request.method == 'POST':
+#         car = Post.objects.get(pk=id)
+#         name = request.POST['name']
+#         comment_text = request.POST['comment']
+#         Comment.objects.create(car=car, user=request.user, name=name, comment=comment_text)
+#         messages.success(request, 'Comment added successfully!')
+#         return redirect('car_details', id=id)  # Redirect to car details using 'id' parameter
+#     else:
+#         return redirect('homepage')  # Redirect to home page if not a POST request
+
+# convert into class vased view : 
+
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, render
-from .models import Post,Order,Comment
+from django.views import View
 from django.contrib import messages
-# Create your views here.
+from .models import Post, Order, Comment
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-@login_required
-def add_post(request):
-    if request.method=='POST':
-        post_form=forms.PostForm(request.POST)
+class AddPostView(View):
+    @login_required
+    def get(self, request):
+        post_form = PostForm()
+        return render(request, 'add_post.html', {'form': post_form})
+    
+    @login_required
+    def post(self, request):
+        post_form = PostForm(request.POST)
         if post_form.is_valid():
-            new_post = post_form.save(commit=False)  # Create a new Post object but don't save to database yet
-            new_post.author = request.user  # Assign the author
-            new_post.save()  # Save the post with the assigned author
+            new_post = post_form.save(commit=False)
+            new_post.author = request.user
+            new_post.save()
             return redirect('add_post')
-    else:
-        post_form=forms.PostForm()
-    return render(request, 'add_post.html',{'form':post_form})
+        return render(request, 'add_post.html', {'form': post_form})
 
-@login_required
-def edit_post(request,id):
-    # post model er instance (/object/children) dakhte parbe ! 
-
-    post=models.Post.objects.get(pk=id)
-    # print(post)
-    post_form=forms.PostForm(instance=post)
-
-    if request.method=='POST':
-        post_form=forms.PostForm(request.POST,instance=post)
-        # user kno kichu change na krle instance... !
+class EditPostView(View):
+    @login_required
+    def get(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        post_form = PostForm(instance=post)
+        return render(request, 'add_post.html', {'form': post_form})
+    
+    @login_required
+    def post(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        post_form = PostForm(request.POST, instance=post)
         if post_form.is_valid():
-            post_form.instance.author=request.user
+            post_form.instance.author = request.user
             post_form.save()
             return redirect('homepage')
-    return render(request, 'add_post.html',{'form':post_form})
+        return render(request, 'add_post.html', {'form': post_form})
 
+class CarDetailsView(View):
+    def get(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        return render(request, 'car_details.html', {'car': post})
 
-def car_details(request, id):
-    post = get_object_or_404(Post, pk=id)
-    return render(request, 'car_details.html', {'car': post})
+class DeletePostView(View):
+    @login_required
+    def post(self, request, id):
+        post = get_object_or_404(Post, pk=id)
+        post.delete()
+        return redirect('homepage')
 
-@login_required
-def delete_post(request,id):
-    post=models.Post.objects.get(pk=id)
-    post.delete()
-    return redirect('homepage')
-@login_required
-def buy_now(request):
-    if request.method == 'POST' and request.user.is_authenticated:
-        car_id = request.POST.get('car_id')
-        if car_id:
-            # Get the car object
-            car = Post.objects.get(id=car_id)
-            # Create a new order for the current user
-            Order.objects.create(car=car, user=request.user)
-            # Decrease the car quantity
-            car.car_quantity -= 1
-            car.save()
-    return redirect('profile')  # Redirect to order history page or user profile
+class BuyNowView(LoginRequiredMixin, View):
+    def post(self, request):
+        if request.user.is_authenticated:
+            car_id = request.POST.get('car_id')
+            if car_id:
+                car = get_object_or_404(Post, id=car_id)
+                Order.objects.create(car=car, user=request.user)
+                car.car_quantity -= 1
+                car.save()
+        return redirect('profile')
+
+# class OrderHistoryView(View):
+#     @login_required
+#     def get(self, request):
+#         orders = Order.objects.filter(user=request.user)
+#         context = {'orders': orders}
+#         return render(request, 'profile.html', context)
 
 @login_required
 def order_history(request):
@@ -69,14 +162,13 @@ def order_history(request):
     return render(request, 'profile.html', context)
 
 
-@login_required
-def add_comment(request, id):
-    if request.method == 'POST':
-        car = Post.objects.get(pk=id)
-        name = request.POST['name']
-        comment_text = request.POST['comment']
-        Comment.objects.create(car=car, user=request.user, name=name, comment=comment_text)
-        messages.success(request, 'Comment added successfully!')
-        return redirect('car_details', id=id)  # Redirect to car details using 'id' parameter
-    else:
-        return redirect('homepage')  # Redirect to home page if not a POST request
+class AddCommentView(LoginRequiredMixin, View):
+    def post(self, request, id):
+        if request.user.is_authenticated:
+            car = get_object_or_404(Post, pk=id)
+            name = request.POST.get('name')
+            comment_text = request.POST.get('comment')
+            Comment.objects.create(car=car, user=request.user, name=name, comment=comment_text)
+            messages.success(request, 'Comment added successfully!')
+            return redirect('car_details', id=id)
+        return redirect('homepage')
